c8d6ce6c7d7f848e2fb9fa59e72078afad9adec1
mnowak00 <marius.nowak@gmail.com>
2018-06-03 15:37:38 +0200

    modified script

--- Cleaningscript.R
+++ Cleaningscript.R
@@ -1,3 +1,7 @@
+## Script to clean two .csv files and finds the sum of tonnes of apples and oranges
+## produced by the top ten politically stable countries
+
+## Reads and cleans the apple and orange .csv table
 aoraw<-read.csv("appleorange.csv",stringsAsFactors = FALSE,header=FALSE)
 ao<-aoraw[3:700,]
 names(ao)<-c("country","countrynumber","product","productnumber","tonnes","year")
@@ -6,21 +10,19 @@ ao$countrynumber<-as.integer(ao$countrynumber)
 ao <- ao[ao$country!="Food supply quantity (tonnes) (tonnes)",]
 ao <- ao %>% mutate(tonnes=parse_number(tonnes))
 ao$year<-2009
+
+## Creates two sorted dataframes: one for apples 'a' and one for oranges 'o'
 a <- ao %>% filter(productnumber=="2617") %>% arrange(desc(tonnes))
 o <- ao %>% filter(productnumber=="2611") %>% arrange(desc(tonnes))
 
-
-
+## Reads and cleans the stability .csv
 sraw <- read.csv("stability.csv",stringsAsFactors = FALSE,header=FALSE)
 s <- sraw[6:960,]
 names(s) <- c('country','countrynumber','factor','factornumber','score','unknown')
 s <- s %>% filter(factor != "") %>% mutate(score=parse_number(score))
 p <- s %>% filter(factornumber=='21032') %>% arrange(score)
 
+## Sums the top ten stable country by produced tonnes of apples and oranges
 sum(a[match(p$country[1:10],a$country),'tonnes'],na.rm=TRUE)
 sum(o[match(p$country[1:10],ao$country),'tonnes'],na.rm=TRUE)
-
-
-##library(dplyr)
-##ao %>% select(c)
 
